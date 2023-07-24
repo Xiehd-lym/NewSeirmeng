21e9fa8f07b6e062d532c432c20deffe95784826
‘Xiehd-lym’ <xiehdwork@163.com>
2023-07-24 10:16:47 +0800

    add demo start time

---
+++ src/main/java/com/haidong/newseirmeng/NewSeirmengApplication.java
@@ -0,0 +1,16 @@
+package com.haidong.newseirmeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 20230724
+ */
+@SpringBootApplication
+public class NewSeirmengApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewSeirmengApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/haidong/newseirmeng/NewSeirmengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haidong.newseirmeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewSeirmengApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
