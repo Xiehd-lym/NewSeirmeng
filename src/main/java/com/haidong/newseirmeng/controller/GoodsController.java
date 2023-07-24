6b651bc0cf22c8a215fedbd9a18b7bc2a7e18403
‘Xiehd-lym’ <xiehdwork@163.com>
2023-07-24 16:51:22 +0800

    druid WebConfig 接口转发页面

--- src/main/java/com/haidong/newseirmeng/controller/GoodsController.java
+++ src/main/java/com/haidong/newseirmeng/controller/GoodsController.java
@@ -3,6 +3,8 @@ package com.haidong.newseirmeng.controller;
 import com.haidong.newseirmeng.entity.GoodsEntity;
 import com.haidong.newseirmeng.service.GoodsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +17,12 @@ public class GoodsController {
     @Autowired
     private GoodsService goodsService;
 
-    @RequestMapping("/list")
+    @GetMapping("/list")
+    public String hello() {
+        return "index";
+    }
+
+    @PostMapping("/list")
     public List<GoodsEntity> list(){
         return goodsService.list();
     }
