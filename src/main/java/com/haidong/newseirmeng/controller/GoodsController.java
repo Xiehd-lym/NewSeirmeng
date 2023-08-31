bcf606e6b1e94161bfdc0ace9ecac79d2fe91a92
‘Xiehd-lym’ <xiehdwork@163.com>
2023-08-31 09:09:13 +0800

    首页更新

--- src/main/java/com/haidong/newseirmeng/controller/GoodsController.java
+++ src/main/java/com/haidong/newseirmeng/controller/GoodsController.java
@@ -7,15 +7,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("goods")
+@RequestMapping("/goods")
 public class GoodsController {
 
     @Autowired
     private GoodsService goodsService;
+
+    @GetMapping("")
+    public ModelAndView helloWeb() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
 
     @GetMapping("/list")
     public String hello() {
